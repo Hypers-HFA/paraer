8ac388c809950b7e3c878f08f482567bf436dd19
drinks <drinksober@foxmail.com>
2017-10-09 00:58:48 +0800

    fix: 升级rest_framework

--- paraer/doc.py
+++ paraer/doc.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 import re
-import json
 
 from openapi_codec import encode
 from openapi_codec.encode import generate_swagger_object as _generate_swagger_object
-from coreapi.compat import force_bytes
-from rest_framework.schemas import SchemaGenerator as _SchemaGenerator
+from rest_framework.schemas import AutoSchema as Schema
 from rest_framework.compat import coreapi
-from rest_framework.renderers import JSONRenderer
-from rest_framework_swagger.renderers import OpenAPICodec as _OpenAPICodec, OpenAPIRenderer as _OpenAPIRenderer
-from rest_framework import status
 from openapi_codec.encode import _get_links
 from django.db import models
 
@@ -19,18 +14,12 @@ from .fields import _get_properties, _callback
 RE_PATH = re.compile('\{(\w+)\}')  # extract  /{arg1}/{arg2}  to [arg1, arg2]
 
 
-class SchemaGenerator(_SchemaGenerator):
-    def get_description(self, path, method, view):
-        method_name = getattr(view, 'action', method.lower())
-        method = getattr(view, method_name, None)
-        doc = method.__doc__
-        self.swagger = getattr(method, '__swagger__',
-                               {})  # para_ok_or_400中生成的__swagger__
-        return doc
-
-    def get_swagger_fields(self, path, method, view):
+class SwaggerSchema(Schema):
+    def get_swagger_fields(self, path, method):
+        swagger = getattr(method, '__swagger__',
+                          {})  # para_ok_or_400中生成的__swagger__
         path_args = {x for x in RE_PATH.findall(path)}
-        parameters = self.swagger.get('parameters', [])
+        parameters = swagger.get('parameters', [])
         fields = tuple(
             coreapi.Field(
                 name=x['name'],
@@ -40,15 +29,12 @@ class SchemaGenerator(_SchemaGenerator):
                 type=x['type']) for x in parameters)
         return fields
 
-    def get_link(self, path, method, view):
+    def get_link(self, path, method, base_url):
         """
         Return a `coreapi.Link` instance for the given endpoint.
         """
-        url = self.url
-        self.url = None  # 当设置url时，生成的path中应该不带host
-        link = super(SchemaGenerator, self).get_link(path, method, view)
-        self.url = url
-        swagger_fields = self.get_swagger_fields(path, method, view)
+        link = super(SwaggerSchema, self).get_link(path, method, base_url)
+        swagger_fields = self.get_swagger_fields(path, method)
         nameset = {x.name for x in swagger_fields}
         fields = tuple(
             (x for x in link.fields
@@ -60,47 +46,22 @@ class SchemaGenerator(_SchemaGenerator):
             fields=fields,
             description=link.description)
         link.__serializer__ = getattr(
-            view, 'serializer_class',
+            self.view, 'serializer_class',
             None)  # Link是不可变的数据结构。。。所以覆盖带下划线的属性,以绕过这个限制
-        link.__action__ = getattr(view, 'action', None)
         return link
 
 
-class OpenAPICodec(_OpenAPICodec):
-    def encode(self, document, extra=None, **options):
-        if not isinstance(document, coreapi.Document):
-            raise TypeError('Expected a `coreapi.Document` instance')
+def wrap_generator(func):
+    def inner(document):
+        swagger = _generate_swagger_object(document)
+        links = _get_links(document)
+        dataset = dict(
+            serializergeter(link.__serializer__) for _, link, _ in links
+            if getattr(link, '__serializer__', None))
+        swagger['definitions'] = dataset
+        return swagger
 
-        data = generate_swagger_object(document)
-        if isinstance(extra, dict):
-            data.update(extra)
-
-        return force_bytes(json.dumps(data))
-
-
-class OpenAPIRenderer(_OpenAPIRenderer):
-    media_type = 'application/openapi+json'
-    charset = None
-    format = 'openapi'
-
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        if renderer_context['response'].status_code != status.HTTP_200_OK:
-            return JSONRenderer().render(data)
-        extra = self.get_customizations()
-
-        return OpenAPICodec().encode(data, extra=extra)
-
-
-def generate_swagger_object(document):
-    """
-    为了生成definitions对象
-    """
-    swagger = _generate_swagger_object(document)
-    links = _get_links(document)
-    dataset = dict(serializergeter(link) for _, link, _ in links if getattr(link, '__serializer__', None))
-    swagger['definitions'] = dataset
-
-    return swagger
+    return inner
 
 
 def _get_serializer_name(serializer):
@@ -141,7 +102,7 @@ def _get200(link):
     elif hasattr(serializer, 'Meta'):
         obj_name = _get_serializer_name(serializer)
     ref = {"$ref": "#/definitions/{}".format(obj_name)}
-    if link.__action__ == 'list':
+    if link.action == 'get' and '{' not in link.url:  # list view
         tpl['schema'] = dict(items=ref, type='array')
     else:
         tpl['schema'] = ref
@@ -175,4 +136,8 @@ def _get_responses(link):
     return response_template
 
 
-encode._get_responses = _get_responses
+def patch_all():
+    import rest_framework
+    rest_framework.schemas.AutoSchema = SwaggerSchema
+    encode._get_responses = _get_responses
+    encode.generate_swagger_object = wrap_generator(_generate_swagger_object)
